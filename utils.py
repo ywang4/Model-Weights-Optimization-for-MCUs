e24f2856e4dfc9250a115020fcfa1d5efed0a30f
Yuan W <wy_cloud@hotmail.com>
2019-01-10 23:32:18 -0800

    save and load model weights

--- utils.py
+++ utils.py
@@ -1,18 +1,37 @@
 from keras.models import load_model
 
 
-def print_model_structure(model_path):
+def print_model_structure(model):
     '''
 
-    :param model_path: The path where the model is saved
+    :param model: a keras model
     :return: a summary of model structure using the keras built-in fuction
     '''
-    model = load_model(model_path)
     model.summary()
 
-def print_model_weights(model_path):
+def load_model_weights(model):
+    # Create two variables to store the layer information and weight matrixes.
+    layers_name = []
+    weights = []
+    
+    # Loop through the model layer by layer
+    for layer in model.layers:
+        # extract the layer information by using layer.get_config()
+        layers_name.append(layer.get_config())
+        
+        # extract the weight matrix for this layer using layer.get_weights()
+        weights.append(layer.get_weights())
+    return weights, layers_name
 
+def save_model_weights(model, new_weights):
+    # Loop through the list containing each layer's new weight
+    for i in range(len(new_weights)):
+        # set the weight for each layer using set_weight()
+        model.layers[i].set_weights(new_weights[i])
+    return model
 
 if __name__ == '__main__':
     model_path = "test_model_files/cifar10_model.h5"
-    print_model_structure(model_path)
+    model = load_model(model_path)
+    # print_model_structure(model)
+    print_model_weights(model)
